d20647e5e49f71b0e38ee8a4f3c95eeba44e838e
Dari <darianadamiled@gmail.com>
2023-07-16 19:51:59 +0200

    01-app variables cambios

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,37 @@
+// Inicializar una variable con un valor
+var producto = "Monitor de 24 Pulgadas";
+console.log(producto);
+
+
+producto = 'Monitor de 19 Pulgadas'; // Podemos reasignamos el valor
+console.log(producto);
+
+// JavaScript es un lenguaje dinámico, no se especifica tipo de dato
+producto = 20;
+console.log(producto);
+
+// Se puede inicializar sin un valor y asignarlo después
+var disponible
+disponible = true;
+
+disponible = false;
+
+// Inicializar múltiples variables
+var categoria = 'Computadoras',
+    marca = "Marca Famosa",
+    calificacion = 5;
+
+// Las variables no pueden iniciar con números
+// var 99dias;
+var dias99;
+
+// var 01_;
+var _01;
+var _usuario;
+var __usuario;
+
+// Camel Case
+var nombreProducto;
+
+// Snake
+var nombre_producto;
