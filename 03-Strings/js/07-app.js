90964ca74a56703f8bf495acc17b989f81eb4d0d
Dari <darianadamiled@gmail.com>
2023-07-18 20:13:40 +0200

    string 06, 07

---
+++ 03-Strings/js/07-app.js
@@ -0,0 +1,11 @@
+const producto = 'Monitor 20 Pulgadas';
+console.log(producto.toUpperCase());
+console.log(producto.toLowerCase());
+// Ejemplo
+const email = "CORREO@CORREO.COM";
+console.log(email.toLowerCase());
+
+// Convertir numeros a String
+const precio = 300;
+console.log(precio);
+console.log(precio.toString());
